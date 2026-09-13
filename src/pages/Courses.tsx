cf508f39c54ce91998b9bbb832ee7fe1567fa862
vkaran1603-stack <vkaran1603@gmail.com>
2026-09-13 09:48:06 +0530

    Update Courses.tsx

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -2,7 +2,7 @@ import { Clock, Users, FileCheck, GraduationCap, BookOpen, FlaskConical } from "
 import bpharmImg from "@/assets/course-bpharm.jpg";
 import dpharmImg from "@/assets/course-dpharm.jpg";
 import mpharmImg from "@/assets/course-mpharm.jpg";
-import veterinaryPharmacyImg from "@/assets/course-veterinaryPharmacyImg";
+import veterinaryPharmacyImg from "@/assets/course-veterinary-pharmacy.jpg";
 
 const courses = [
   {
